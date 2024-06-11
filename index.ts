2dfb6dd095dec09c9f15372b07ad9a043be0be22
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-11 14:58:36 +0200

    Added properties to Dashboard

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+const propertyContainer = document.querySelector('.properties')
 import { showReviewTotal, populateUser } from './utils'
 
 let isOpen : boolean
@@ -41,8 +42,73 @@ const you = {
     isReturning: true,
     age: 35
     stayedAt: ['florida-home', 'oman-flat'. 'tokyo-bungalow']
+
 }
 
+const properties : {
+    image: string;
+    title: string;
+    price: number;
+    location: {
+        firstLine: string;
+        city: number;
+        country: string;  
+    };
+    contact: string;
+    isAvailable: boolean;
+}[] = [
+    {
+        image: '',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: 'TankayaSue@gmail.com',
+        isAvailable: true
 
+    },
+    {
+        image: '',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 99567,
+            country: 'Poland'
+        },
+        contact: 'Garymavsk@hotmail.com',
+        isAvailable: false
+
+    }
+    {
+        image: '',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 354333,
+            country: 'United Kingdom'
+        },
+        contact: 'TomCruiz@hotmail.com',
+        isAvailable: true
+
+    }
+]
 showReviewTotal('two', reviews[0].name, reviews[0].loyaltyUser)
+
 populateUser(you.isReturning, you.firstName)
+
+for (let i=0; i < propertiers.length; i++) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = properties[i].title
+    const image = document.createElement('img')
+    image.setAttribute('src', properties[i].image)
+    card.appendChild(image)
+    propertyContainer.appendChild(card)
