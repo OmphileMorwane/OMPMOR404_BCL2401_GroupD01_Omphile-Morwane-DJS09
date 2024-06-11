e5f4cd707061c11a92b63dac32dba1ebc9122b92
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-11 20:30:40 +0200

    "Deleted files: images, index.css, index.html. Modified files: enums.ts, index.ts, webpack.config.js. Removed CSS styles, HTML content, and image references. Updated enums, index logic, and Webpack configuration."

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,9 @@ const stylesHandler = isProduction ? MiniCssExtractPlugin.loader : 'style-loader
 
 
 const config = {
-    entry: './src/index.ts',
+    entry: {
+        index: './src/index.ts',
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
     },
