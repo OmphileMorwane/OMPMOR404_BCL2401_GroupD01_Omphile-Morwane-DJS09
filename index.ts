df7111c08f7d32c838c30c0778c960e10c68bb85
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-11 15:11:04 +0200

    Fixing the syntax

--- index.ts
+++ index.ts
@@ -1,64 +1,67 @@
+
 const propertyContainer = document.querySelector('.properties')
+
 import { showReviewTotal, populateUser } from './utils'
+let isOpen: boolean
 
-let isOpen : boolean
+// Reviews
+const reviews : { 
+    name: string; 
+    stars: number; 
+    loyaltyUser: boolean; 
+    date: string
+    }[] = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
 
-const reviews : {
-    name: string;
-    stars: number;
-    loyaltyUser : boolean;
-    date: string;
-}[] = [
-  {
-    name: "Sheia",
-    stars: 5,
-    loyaltyUser: true,
-    date: "01-04-2021",
-  },
-  {
-    name: "Andrzej",
-    stars: 3,
-    loyaltyUser: false,
-    date: "28-03-2021",
-  },
-  {
-    name: "Omar",
-    stars: 4,
-    loyaltyUser: true,
-    date: "27-03-2021",
-  },
-];
-
-
-const you = {
+// User
+const you: {
     firstName: string;
     lastName: string;
     isReturning: boolean;
     age: number;
-    stayedAt: (string)[];
-}  = {
+    stayedAt: string[]
+} = {
     firstName: 'Bobby',
     lastName: 'Brown',
     isReturning: true,
-    age: 35
-    stayedAt: ['florida-home', 'oman-flat'. 'tokyo-bungalow']
-
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
 }
 
+// Array of Properties
 const properties : {
     image: string;
     title: string;
     price: number;
     location: {
         firstLine: string;
-        city: number;
-        country: string;  
+        city: string;
+        code: number;
+        country: string;
     };
     contact: string;
     isAvailable: boolean;
 }[] = [
     {
-        image: '',
+        image: 'images/colombia-property.jpg',
         title: 'Colombian Shack',
         price: 45,
         location: {
@@ -67,44 +70,44 @@ const properties : {
             code: 45632,
             country: 'Colombia'
         },
-        contact: 'TankayaSue@gmail.com',
-        isAvailable: true
-
+        contact: 'marywinkle@gmail.com',
+        isAvailable: true  
     },
     {
-        image: '',
+        image: 'images/poland-property.jpg',
         title: 'Polish Cottage',
         price: 34,
         location: {
             firstLine: 'no 23',
             city: 'Gdansk',
-            code: 99567,
+            code: 343903,
             country: 'Poland'
         },
-        contact: 'Garymavsk@hotmail.com',
-        isAvailable: false
-
-    }
+        contact: 'garydavis@hotmail.com',
+        isAvailable: false 
+    },
     {
-        image: '',
+        image: 'images/london-property.jpg',
         title: 'London Flat',
         price: 23,
         location: {
             firstLine: 'flat 15',
             city: 'London',
-            code: 354333,
-            country: 'United Kingdom'
+            code: 35433,
+            country: 'United Kingdom',
         },
-        contact: 'TomCruiz@hotmail.com',
+        contact: 'andyluger@aol.com',
         isAvailable: true
-
     }
 ]
-showReviewTotal('two', reviews[0].name, reviews[0].loyaltyUser)
+
+// Functions
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
 populateUser(you.isReturning, you.firstName)
 
-for (let i=0; i < propertiers.length; i++) {
+//Add the properties
+for (let i = 0; i < properties.length; i++) {
     const card = document.createElement('div')
     card.classList.add('card')
     card.innerHTML = properties[i].title
@@ -112,3 +115,4 @@ for (let i=0; i < propertiers.length; i++) {
     image.setAttribute('src', properties[i].image)
     card.appendChild(image)
     propertyContainer.appendChild(card)
+}
