27395eeae76d1b9f394b7e2557bafc911a59b149
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-11 11:59:02 +0200

    Assigning names 2 returning and nonreturning users

--- index.ts
+++ index.ts
@@ -1,7 +1,4 @@
-// Number Types mini-challenge 10 10.2
-// Write a function that will only accept numbers and attend to
-// all TypeScript weakness flags.
-// : number
+
 const reviewTotalDisplay = document.querySelector("#reviews");
 
 const reviews = [
@@ -32,3 +29,21 @@ function showReviewTotal(value : number, reviewer: string, isLoyalty : boolean) 
 }
 
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+
+
+
+const you = {
+    userName: 'Bobby'
+    isReturning: true,
+}
+
+
+function populateUser(isReturning : boolean, userName : string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.userName)
