57e000addbafe0c43899a4635fa121de170a6724
Omphile Morwane <omphilemorwane@gmail.com>
2024-06-11 14:27:28 +0200

    Moved functions and query selectors to utils file

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
-const returningUserDisplay = document.querySelector('#returning-user')
-const userNameDisplay = document.querySelector('#user')
-const reviewTotalDisplay = document.querySelector("#reviews");
+import { showReviewTotal, populateUser } from './utils'
 
 let isOpen : boolean
 
@@ -30,17 +28,6 @@ const reviews : {
   },
 ];
 
-function showReviewTotal(value : number, reviewer: string, isLoyalty : boolean) {
-    const iconDisplay = isLoyalty ? '⭐' : ''
-  reviewTotalDisplay.innerHTML = 'review total' + value.toString() + '| last reviewed by ' +
-  reviewer + ' ' + iconDisplay
-}
-
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
-
-let stayedAt = string[]
-stayedAt
-
 
 const you = {
     firstName: string;
@@ -57,11 +44,5 @@ const you = {
 }
 
 
-function populateUser(isReturning : boolean, userName : string ) {
-    if (isReturning){
-        returningUserDisplay.innerHTML = 'back'
-    }
-    userNameDisplay.innerHTML = userName
-}
-
-populateUser(you.isReturning, you.userName)
+showReviewTotal('two', reviews[0].name, reviews[0].loyaltyUser)
+populateUser(you.isReturning, you.firstName)
